71caca3e4e325f6e5bc363a161a6597001a2975b
Yilman0806 <tatobru8@gmail.com>
2024-06-23 22:49:58 -0500

    Se agrega el proyecto del SENA

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Vista.Vista_Menu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vista_Menu vista = new Vista_Menu();
+        vista.Conectar();
+
+    }
+}
